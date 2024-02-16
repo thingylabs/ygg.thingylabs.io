c6f7887f5e3095e3394855e2dae03ba154709859
Per Guth <mail@perguth.de>
2024-02-16 11:49:40 +0800

    Make the button react

--- dist/x-nav.js
+++ dist/x-nav.js
@@ -3,16 +3,13 @@
   
 
   
-  if (document.location.host.includes('-only')) {
-    document.getElementById('ygg-switch').href = 'https://ygg.thingylabs.io/'
-    document.getElementById('ygg-switch').innerHTML = '🔌'
-  }
-
 
   const __template = function({ state }) {
     return [  
     h("div", {"class": `d-flex flex-row-reverse p-2`}, [
-      h("a", {"href": `https://ygg-only.thingylabs.io/`, "id": `ygg-switch`, "class": `btn btn-light`}, `⚡`),
+      h("a", {"href": `https://${!isYgg ? 'ygg-only' : 'ygg'}.thingylabs.io/`, "id": `ygg-switch`, "class": `btn btn-light`}, `${
+      !isYgg ? '⚡' : '🔌'
+    }`),
       h("a", {"href": `https://publicpeers.neilalexander.dev/`, "class": `btn btn-light`}, `Public Peers`),
       h("a", {"href": `speedtest.html`, "class": `btn btn-light`}, `Speedtest`),
       h("a", {"href": `uplink.html`, "class": `btn btn-light`}, `Uplink`),
@@ -25,11 +22,14 @@
     return h('style', {}, `
       
       
+      @import url(bootstrap.purged.css);
+      @import url(style.css);
+    
     `)
   }
 
   // -- Lego Core
-  export default class Lego extends Component {
+  class Lego extends Component {
     init() {
       this.useShadowDOM = false
       if(typeof state === 'object') this.__state = Object.assign({}, state, this.__state)
@@ -43,3 +43,10 @@
   // -- End Lego Core
 
   
+  let isYgg
+  export default class extends Lego {
+    init() {
+      isYgg = document.location.host.includes('-only')
+    }
+  }
+
