a24dcff70de536c440dea913040003102df7afef
Per Guth <mail@perguth.de>
2024-02-16 16:48:09 +0800

    Revert "Rm speedtest page"
    This reverts commit b92c3caa1b71e632ff4c607bef47038ba7a7b079.

--- dist/x-nav.js
+++ dist/x-nav.js
@@ -11,6 +11,7 @@
       !isYgg ? '⚡' : '🔌'
     }`),
       h("a", {"href": `https://publicpeers.neilalexander.dev/`, "class": `btn btn-light`}, `Public Peers`),
+      h("a", {"href": `speedtest.html`, "class": `btn btn-light`}, `Speedtest`),
       h("a", {"href": `uplink.html`, "class": `btn btn-light`}, `Uplink`),
       h("a", {"href": `index.html`, "class": `btn btn-light`}, `Home`)
     ])
