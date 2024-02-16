33d7c510ac6941816e6e87723af8035d2be05eef
Per Guth <mail@perguth.de>
2024-02-16 21:43:41 +0800

    Fix the terminal background color

--- change-background-color.js
+++ change-background-color.js
@@ -1,12 +1,11 @@
-document.addEventListener('load', function () {
-  const iframes = document.getElementsByTagName("iframe")
-  for (const iframe of iframes) {
-    iframe.addEventListener('load', function (event) {
-      const styleElem = document.createElement('style')
-      styleElem.textContent = `.xterm-viewport {
-        background-color: rgb(37, 42, 51) !important;
-      }`
-      event.target.contentDocument.head.appendChild(styleElem)
-    })
-  }
-})
+const iframes = document.getElementsByTagName('iframe')
+for (const iframe of iframes) {
+  const styleElem = document.createElement('style')
+  styleElem.textContent = `
+    #terminal, .xterm-viewport, .react-terminal-wrapper {
+      background-color: rgb(37, 42, 51) !important;
+    }
+  `
+  iframe.contentDocument.head.appendChild(styleElem)
+  iframe.style = 'display: block;'
+}
