3d0ca0695ce33b474ab245ebc4ea4ea41b1e74a4
Per Guth <mail@perguth.de>
2024-02-17 13:35:36 +0000

    Look for iframe content before CSS update

--- change-background-color.js
+++ change-background-color.js
@@ -1,5 +1,27 @@
 const iframes = document.getElementsByTagName('iframe')
 for (const iframe of iframes) {
+  findContent().then(_ => {
+    appendStyle(iframe)
+  })
+}
+
+function findContent () {
+  return new Promise(done => {
+    search(done)
+  })
+
+  function search (done, isRetry) {
+    setTimeout(_ => {
+      if (!iframes[0].contentDocument || !iframes[0].contentDocument.body.querySelector('#terminal')) {
+        search(done, true)
+        return
+      }
+      done()
+    }, isRetry ? 50 : 1)
+  }
+}
+
+function appendStyle (iframe) {
   const styleElem = document.createElement('style')
   styleElem.textContent = `
     #terminal, .xterm-viewport, .react-terminal-wrapper {
