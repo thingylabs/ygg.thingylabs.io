64e2a68b40b6b42a9f011255c1116bf9e603f9b2
Per Guth <mail@perguth.de>
2024-02-19 07:49:10 +0000

    Turn footer into brick

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
-import XButtons from './x-buttons.js'
-customElements.define('x-buttons', XButtons)
+import XFooter from './x-footer.js'
+customElements.define('x-footer', XFooter)
 
 import XNav from './x-nav.js'
 customElements.define('x-nav', XNav)
+
+import XButtons from './x-buttons.js'
+customElements.define('x-buttons', XButtons)
---
+++ dist/x-footer.js
@@ -0,0 +1,59 @@
+ // Lego version 2.0.0-beta.8
+  import { h, Component } from './lego.min.js'
+  
+
+  
+
+  const __template = function({ state }) {
+    return [  
+    h("div", {"id": `footer`}, [
+      h("ul", {}, [
+        h("li", {}, [
+          h("a", {"href": `https://www.thingylabs.io/impressum.html`}, `Impressum`)
+        ]),
+        h("li", {},           h("a", {"href": `https://yggdrasil-network.github.io/about.html`}, `Non-commercial research projct`))
+      ])
+    ])
+  ]
+  }
+
+  const __style = function({ state }) {
+    return h('style', {}, `
+      
+      
+      #footer {
+        width: 100%;
+        padding: 0 20px;
+        font-size: smaller;
+      }
+      #footer ul {
+        text-align: right;
+        margin: 0;
+      }
+      #footer li {
+        margin-left: 30px;
+        float: right;
+        display: list-item;
+      }
+      #footer a {
+        color: rgb(37, 42, 51);
+      }
+    
+    `)
+  }
+
+  // -- Lego Core
+  export default class Lego extends Component {
+    init() {
+      this.useShadowDOM = false
+      if(typeof state === 'object') this.__state = Object.assign({}, state, this.__state)
+      if(typeof methods === 'object') Object.keys(methods).forEach(methodName => this[methodName] = methods[methodName])
+      if(typeof connected === 'function') this.connected = connected
+      if(typeof setup === 'function') setup.bind(this)()
+    }
+    get vdom() { return __template }
+    get vstyle() { return __style }
+  }
+  // -- End Lego Core
+
+  
