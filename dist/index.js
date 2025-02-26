a97e505515e07b09c537576b02cfcf3e0a43ce71
idrathernotgetmails <idrathernotgetmails@gmail.com>
2025-02-26 04:16:41 +0000

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 import XFooter from './x-footer.js'
 customElements.define('x-footer', XFooter)
 
-import XNav from './x-nav.js'
-customElements.define('x-nav', XNav)
-
 import XButtons from './x-buttons.js'
 customElements.define('x-buttons', XButtons)
+
+import XNav from './x-nav.js'
+customElements.define('x-nav', XNav)
